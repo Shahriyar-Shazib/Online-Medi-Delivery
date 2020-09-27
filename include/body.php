8f0b92058eb1a47293b630889b4205442be10b8f
Shahriyar <68555053+Shahriyar-Shazib@users.noreply.github.com>
2020-09-27 23:28:06 +0600

    Add files via upload

---
+++ include/body.php
@@ -0,0 +1,71 @@
+<section >
+      
+      <div class="container body ">
+      <div class="row">
+  <div class="col-sm-3 card1">
+    <div class="card"style="width: 18rem;">
+    <img class="card-img-top" src="include\payment.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Payment the next day</h5>
+        <p class="card-text">Payment is made directly through bank / bKash on the day after order delivery.</p>
+        
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3 card2">
+    <div class="card"style="width: 18rem;">
+        <img class="card-img-top" src="include\cod.jpg" alt="Card image cap"> 
+      <div class="card-body">
+        <h5 class="card-title">0% COD charge</h5>
+        <p class="card-text">There is no "cash on delivery" charge for delivery within Bangladesh.</p>
+        
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3 card3">
+    <div class="card"style="width: 18rem;">
+        <img class="card-img-top" src="include\bdmap.jpg" alt="Card image cap"> 
+      <div class="card-body">
+        <h5 class="card-title">Nationwide delivery service</h5>
+        <p class="card-text">Home delivery facility in 400+ police station areas of the country including Dhaka</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+      </div>
+      <div class="container body ">
+      <div class="row ">
+  <div class="col-sm-3 card4">
+    <div class="card"style="width: 18rem;">
+    <img class="card-img-top" src="include\real time'.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Real time order tracking</h5>
+        <p class="card-text">Real-time order tracking lets you know the latest updates on each order, anytime.</p>
+        
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3 card5">
+    <div class="card"style="width: 18rem;">
+        <img class="card-img-top" src="include\fastdelivery.jpg" alt="Card image cap"> 
+      <div class="card-body">
+        <h5 class="card-title">Fast delivery</h5>
+        <p class="card-text">We have 1,500 skilled deliverymen who complete deliveries nationwide in the fastest time.</p>
+        
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3 card6">
+    <div class="card"style="width: 18rem;">
+        <img class="card-img-top" src="include\bikash.png" alt="Card image cap"> 
+      <div class="card-body">
+        <h5 class="card-title">BKASH payment facility</h5>
+        <p class="card-text">For the convenience of delivery payment, the customer can now make delivery payment through cash on delivery and BKASH, right at the doorstep!</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+      </div>
+</section>
---
+++ include/footer.php
@@ -0,0 +1,71 @@
+
+<footer>
+  <div class="coverage"style="background-color: lightslategray;">
+
+ 
+    <div class="container" >
+        <div class="footer-head">
+        <div class="row r1">
+        <div class="col">
+          <div class="head7">
+            <p><b>Important links</b></p>
+            
+          </div>
+        </div>
+        <div class="col">
+          <div class="head7">
+            <p><b>Contact</b></p>
+          </div>
+        </div>
+        <div class="col">
+        <div class="head7">
+            <p><b>Address</b></p>
+          </div>
+        </div>
+        <div class="col">
+        <div class="head7">
+            <p><b>About</b></p>
+          </div>
+        </div>
+          
+         
+          </div>
+        </div>
+        <div class="footer-subhead7">
+          <div class="row r1">
+        <div class="col">
+          <div class="sub-head7">
+            <p><a href="#">privacy policy</a><br><a href="#">about us</a>
+          <br><a href="#">coverage area</a></p>
+          </div>
+        </div>
+        <div class="col">
+          <div class="sub-head7">
+            <p>01956-424568 <br>01861-670601 <br>shazib.shahriyar@gmail.com <br>ishratjahan@gmail.com</p>
+          </div>
+        </div>
+        <div class="col">
+        <div class="sub-head7">
+            <p>12/A-road.no:3B-Sector:9 <br> Gulshan-2 1200 Bangladesh</p>
+          </div>
+        </div>
+        <div class="col">
+        <div class="sub-head7">
+            <p>this is a demo project we could not impliment it properly
+            for time shortge and some other lackege we will work further for
+            better user experience </p>
+          </div>
+        </div>
+      </div>
+      </div>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <script src="js\app.js"></script>
+    </footer>
+
+    
+      
+    
+   
+    
+</html>
